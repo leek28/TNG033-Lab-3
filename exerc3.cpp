0962e510229bb5dacbdebcf06f91da1c00011775
leek28 <alela841@student.liu.se>
2024-12-18 16:50:20 +0100

    Add files via upload

---
+++ exerc3.cpp
@@ -0,0 +1,39 @@
+/**************************
+ * Lab 3: exercise 3      *
+ **************************/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+
+int main() {
+	std::string word; 
+	std::map<std::string, std::set<std::string>> anagrams; 
+
+	std::ifstream readFile("C:/Users/alars/Documents/TNG033/lab3/code/uppgift3.txt");
+	std::ofstream writeFile("C:/Users/alars/Documents/TNG033/lab3/code/test.txt"); 
+	
+	if (readFile.is_open()) {
+		while (readFile >> word) {
+			std::string subject = word;
+			std::sort(subject.begin(), subject.end());
+			anagrams[subject].insert(word);
+		}
+		readFile.close();
+	}
+	else std::cout << "Could not open file!! nononono"; 
+
+	writeFile << "-- ANAGRAMS --" << std::endl; 
+	for (const auto& a : anagrams) {
+		for (std::string s : a.second) {
+			if (a.second.size() > 1) writeFile << s << " "; 
+		}
+		if (a.second.size() > 1) writeFile << " --> " << a.second.size() << " words.\n";
+	}
+	writeFile.close(); 
+}
+
